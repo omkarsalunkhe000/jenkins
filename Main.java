a13832cff4d93bf936cc7146c69ee58ad9be3934
Omkar Salunkhe <94234311+omkarsalunkhe000@users.noreply.github.com>
2025-07-13 22:55:49 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Main{
+        public static void main(String[] args) {
+            System.out.println("This is java proggram ");
+            System.out.println("This is output from java");
+            System.out.println(new Date());
+            System.out.println("Omkar");
+            System.out.println("Omkar");
+            System.out.println("Omkar");
+        }
+}
